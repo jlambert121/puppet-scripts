0b317f190e780df9daee46910e9bc0ed965dba97
Joe McDonagh <jmcdonagh@thesilentpenguin.com>
2012-04-24 10:55:12 -0400

    Fix for resize2fs not running

--- ec2-instance.rb
+++ ec2-instance.rb
@@ -323,7 +323,7 @@ if puppetize == true
             sleep 15
 
             if volumesize > 8
-               `ssh #{i.user_data} 'sudo resize2fs -f /dev/sda1'`
+               `ssh bmadmin@#{i.user_data} 'sudo resize2fs -f /dev/sda1'`
             end
 
             `./sendkey.sh new #{i.user_data}`
