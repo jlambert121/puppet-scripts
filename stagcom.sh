42e416658894a13bb0d042755da1617612302ee3
Joe McDonagh <jmcdonagh@thesilentpenguin.com>
2012-09-27 11:47:22 -0400

    [#5016] Git hooks and stagcom update

--- stagcom.sh
+++ stagcom.sh
@@ -9,9 +9,14 @@ if [ -z "$COMMIT_MESSAGE" ]; then
    exit -1
 fi
 
-git submodule foreach 'if [[ $path =~ ^staging.* ]]; then git checkout develop; git add .; git commit -am "$COMMIT_MESSAGE"; git push -u origin develop; fi'
+git submodule foreach 'if [[ $path =~ ^staging.* ]]; then git checkout develop; git add .; git commit -am "$COMMIT_MESSAGE" && git push -u origin develop; fi'
 
-git commit -am "Updated submodules: $COMMIT_MESSAGE"
-git push
+if [ $? -eq 0 ]; then
+   git commit -am "Updated submodules: $COMMIT_MESSAGE" && git push && exit 0
+   exit $?
+else
+   printf "Error committing or pushing in submodules!\n" >&2
+   exit -1
+fi
 
 #vim: set expandtab ts=3 sw=3:
